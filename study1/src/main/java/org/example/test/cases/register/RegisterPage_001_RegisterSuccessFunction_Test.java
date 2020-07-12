de32af8147e8ad310ab03f00339ba740d98b022a
qq969887453 <969887453@qq.com>
2020-07-12 18:22:08 +0800

    update

--- study1/src/main/java/org/example/test/cases/register/RegisterPage_001_RegisterSuccessFunction_Test.java
+++ study1/src/main/java/org/example/test/cases/register/RegisterPage_001_RegisterSuccessFunction_Test.java
@@ -3,6 +3,7 @@ package org.example.test.cases.register;
 import org.apache.log4j.Logger;
 import org.example.test.base.BaseParpare;
 import org.example.test.pagehelper.RegisterPageHelper;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.util.Map;
@@ -18,9 +19,15 @@ class RegisterPage_001_RegisterSuccessFunction_Test extends BaseParpare {
                 "zhang", "san","shenzhen","nanshan");*/
         /*RegisterPageHelper.register(seleniumUtil, timeOut,user);*/
         System.out.println(data.size());
-        Map<String, String> cases = (Map<String, String>) data.get(1);
-        RegisterPageHelper.typeRegisterInfo(seleniumUtil,cases.get("USERNAME"),cases.get("PASSWORD"),
-                cases.get("Confirm"),cases.get("First Name"),cases.get("Last Name"),cases.get("Street Address"),
-                cases.get("City/State/Zip"));
+        if (data != null){
+            for (Object key: data.keySet()){
+                Map<String, String> cases = (Map<String, String>) data.get(key);
+                RegisterPageHelper.typeRegisterInfo(seleniumUtil,cases.get("USERNAME"),cases.get("PASSWORD"),
+                        cases.get("Confirm"),cases.get("First Name"),cases.get("Last Name"),cases.get("Street Address"),
+                        cases.get("City/State/Zip"));
+            }
+        }else {
+            Assert.fail("数据为空，请重准备数据！");
+        }
     }
 }
